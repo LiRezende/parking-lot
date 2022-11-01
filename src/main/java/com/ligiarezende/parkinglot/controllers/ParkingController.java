f1d23f523120cfd05d27dc1a72b2f11134fe4523
LiRezende <librusamolin@gmail.com>
2022-11-01 08:04:47 -0300

    Find by id configured.

--- src/main/java/com/ligiarezende/parkinglot/controllers/ParkingController.java
+++ src/main/java/com/ligiarezende/parkinglot/controllers/ParkingController.java
@@ -4,10 +4,7 @@ import com.ligiarezende.parkinglot.dto.ParkingDTO;
 import com.ligiarezende.parkinglot.services.ParkingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/parking")
@@ -19,6 +16,12 @@ public class ParkingController {
     @PostMapping
     public ResponseEntity<ParkingDTO> create(@RequestBody ParkingDTO dto) {
         dto = parkingService.create(dto);
+        return ResponseEntity.ok().body(dto);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ParkingDTO> findById(@PathVariable Long id) {
+        ParkingDTO dto = parkingService.findById(id);
         return ResponseEntity.ok().body(dto);
     }
 }
